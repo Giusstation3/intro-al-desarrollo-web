f5a9e91fdba4b3c0748becdf0dbc6aae8e592d0c
Gius <102930878+Giusstation3@users.noreply.github.com>
2025-09-19 09:59:19 -0600

    Create funciones_mult_sqrd.js

---
+++ funciones_mult_sqrd.js
@@ -0,0 +1,17 @@
+function mult_classic(num1,num2) {
+	return num1*num2;
+}
+
+const mult_arrow = (num1,num2) => {
+  return num1*num2;
+};
+
+function sqrd_classic(num1){
+	return num1*num1;
+}
+
+}
+
+const sqrd_arrow= (num1) => {
+  return num1*num1;
+};
